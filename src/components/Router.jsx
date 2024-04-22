04fd1e8ca88e8b2204e8c7c18791246653acd8f0
yna <ynaooro@gmail.com>
2024-04-22 11:15:13 +0300

    made arouter

---
+++ src/components/Router.jsx
@@ -0,0 +1,24 @@
+import {BrowserRouter,Routes,Route} from "react-router-dom"
+import Login from "../pages/Login"
+import Signup from "../pages/Signup"
+
+function Router (){
+    return(
+      <BrowserRouter>
+<Route>
+<Route path="/login"element={<Layout><Login/></Layout>}/>
+<Route path="/signup"element={<Layout><Signup/></Layout>}/>
+<Route path="/payment"element={<Layout><Signup/></Layout>}/>
+
+
+</Route>
+
+
+
+
+
+
+      </BrowserRouter>  
+    )
+}
+export default Router
